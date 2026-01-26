e4ac5ee4b517a485a37242c7bd1e8ff217b0a680
Gabriel Xavier <gabrielluck492@gmail.com>
2026-01-26 14:30:27 -0300

    Now is possible to do simple calculations as sum, diff, mult, etc v0.0.1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import ProjetoCalculator.InputLogic;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean running = true;
+
+        while(running){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Select one of the options below" +
+                    "\n 1- simple calculation" +
+                    "\n 0 - exit");
+            byte option = sc.nextByte();
+            if(option == 1){
+                new InputLogic();
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
--- src/ProjetoCalculator/InputLogic.java
+++ src/ProjetoCalculator/InputLogic.java
@@ -1,4 +1,48 @@
 package ProjetoCalculator;
 
+import java.util.Scanner;
+
 public class InputLogic {
+    //Basis functions
+    private static double sum(String  number1,String  number2){
+        return Double.parseDouble(number1) + Double.parseDouble(number2);
+    }
+    private static double difference(String  number1,String  number2){
+        return Double.parseDouble(number1) - Double.parseDouble(number2);
+    }
+    private static double multiply(String  number1,String  number2){
+        return Double.parseDouble(number1) * Double.parseDouble(number2);
+    }
+    private static double division(String  number1,String  number2){
+        if(number2.equals("0")){
+            number2 = "1";
+        }
+        return Double.parseDouble(number1) / Double.parseDouble(number2);
+    }
+
+    public InputLogic(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the numbers that will be calculated");
+        String number1 = sc.nextLine();
+        String number2 = sc.nextLine();
+        System.out.println("""
+                Choose e of the options below\
+                
+                 1- Add\
+                
+                 2- diff\
+                 
+                 3- multiply\
+                 
+                 4- division              
+                """);
+        byte option = sc.nextByte();
+        switch (option){
+            case 1 -> System.out.println(sum(number1, number2));
+            case 2 -> System.out.println(difference(number1, number2));
+            case 3 -> System.out.println(multiply(number1, number2));
+            case 4 -> System.out.println(division(number1, number2));
+            default -> throw new IllegalStateException("invalid option: " + option);
+        }
+    }
 }
